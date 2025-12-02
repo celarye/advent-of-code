7f8c6796d9583183458d0e3cda64fd2c363d2a1a
Eduard Smet <contact@celarye.dev>
2025-12-02 01:02:35 +0100

    feat(2025): Day 1

--- 2025/src/days/_1.rs
+++ 2025/src/days/_1.rs
@@ -1,9 +1,65 @@
-pub fn part1(_input: String) -> String {
-    "Not Implemented".to_string()
+pub fn part1(input: String) -> String {
+    let mut value = 50;
+    let mut match_count = 0;
+
+    for input_line in input.split("\n") {
+        if input_line.is_empty() {
+            continue;
+        }
+
+        let (direction, count_str) = input_line.split_at(1);
+
+        let mut count = count_str
+            .parse::<i32>()
+            .expect("the count str should parse into valid i32");
+
+        if direction == "L" {
+            count *= -1;
+        }
+
+        value += count;
+
+        if value % 100 == 0 {
+            match_count += 1;
+        }
+    }
+
+    match_count.to_string()
 }
 
-pub fn part2(_input: String) -> String {
-    "Not Implemented".to_string()
+pub fn part2(input: String) -> String {
+    let mut value = 50;
+    let mut match_count = 0;
+
+    for input_line in input.split("\n") {
+        if input_line.is_empty() {
+            continue;
+        }
+
+        let (direction, count_str) = input_line.split_at(1);
+
+        let mut count = count_str
+            .parse::<i32>()
+            .expect("the count str should parse into valid 32");
+
+        if direction == "L" {
+            count *= -1;
+        }
+
+        match_count += (count / 100).abs();
+
+        if (value + (count % 100)) % 100 == 0
+            || value % 100 != 0
+                && (value / 100 != (value + (count % 100)) / 100
+                    || value.is_positive() != (value + (count % 100)).is_positive())
+        {
+            match_count += 1;
+        }
+
+        value += count;
+    }
+
+    match_count.to_string()
 }
 
 #[cfg(test)]
@@ -12,15 +68,15 @@ mod tests {
 
     #[test]
     fn part1_test() {
-        let input = fs::get_input(0, true).expect("there needs to be an example input file");
+        let input = fs::get_input(1, true).expect("there needs to be an example input file");
 
-        assert_eq!("Part 1 Example Input Result".to_string(), _1::part1(input))
+        assert_eq!("3", &_1::part1(input));
     }
 
     #[test]
     fn part2_test() {
-        let input = fs::get_input(0, true).expect("there needs to be an example input file");
+        let input = fs::get_input(1, true).expect("there needs to be an example input file");
 
-        assert_eq!("Part 2 Example Input Result".to_string(), _1::part2(input))
+        assert_eq!("6", &_1::part2(input));
     }
 }
--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Progress: 12/50
 
 ## 2025
 
-Progress: 0/2
+Progress: 2/2
